47eac3c1d4192cfc52da873ff95d341f8c218d95
Anish Kumar Singh <133130608+Anish494@users.noreply.github.com>
2024-06-14 22:14:42 +0545

    Add files via upload

---
+++ resp.js
@@ -0,0 +1,11 @@
+burger=document.querySelector('.burger')
+right=document.querySelector('.right')
+// burger=document.querySelector('.burger')
+// burger=document.querySelector('.burger')
+
+
+burger.addEventListener('click',()=>{
+    right.classList.toggle('v');
+    nav.classList.toggle('hnav');
+    
+})
